7a9785288431c5ffeb0f7f1a07c8eddc2aefb5ff
wengwengweng <593110501@qq.com>
2017-06-27 17:08:59 +0800

    udpate readme

--- middle/router/index.js
+++ middle/router/index.js
@@ -14,22 +14,20 @@ router.post('/', async (ctx, next) => {
 
   // thrift文件中定义的ping方法
   await client.ping(function(err, response) {
-    console.log('ping in middle\n');
+    console.log('ping in middle');
   });
 
   const date = Date.parse(new Date());
 
   // thrift文件中定义的save方法
-  // async与promise同时使用，等待回调函数执行完后返回response
-  function save(){
-    return new Promise((resolve, reject) => {
-      client.save(ctx.request.body.username, ctx.request.body.words, String(date), function(err, response) {
-        console.log("res:",response);
-        resolve(response)
-      });
-    })
-  }
-  ctx.body = await save();
+  // 使用promise，等待回调函数执行完后返回response
+  // 方式可能不是很优雅，之后可能进行封装
+  ctx.body = await new Promise((resolve, reject) => {
+    client.save(ctx.request.body.username, ctx.request.body.words, String(date), function(err, response) {
+      console.log("middle get response from server:",response);
+      resolve(response)
+    });
+  })
 
   // 返回给前端的参数
   return ctx.body;
--- server/app.js
+++ server/app.js
@@ -4,16 +4,23 @@ var ttypes = require("../thrift/gen-nodejs/list_types");
 var Data = require("../thrift/gen-nodejs/list_types").Data;
 var List_Item = require("../thrift/gen-nodejs/list_types").List_Item;
 
+// 直接定义一个变量用来存储客户端发送的数据
 var list = [];
 
+// 创建一个thrift服务
 var server = thrift.createServer(SaveList, {
+
+  // 在idl中定义的ping方法
   ping: function(result) {
-    console.log("ping in server\n");
+    console.log("ping in server");
     result(null);
   },
 
+  // 在idl中定义的save方法
   save: function(name, words, date, result){
-    console.log('data:', name, words, date);
+    console.log('server get data from middle:', name, words, date);
+
+    // 在给list push元素时需要调用 idl中的new方法，不然在new Data()时会报错
     list.push(
       new List_Item({
         name: name,
@@ -21,6 +28,8 @@ var server = thrift.createServer(SaveList, {
         date: date
       })
     )
+
+    // 定义返回值并返回
     const data = new Data({
       code:'S01',
       msg:'success',
